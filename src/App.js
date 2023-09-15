2b255f54bd4034715cb2ad8bd6933a6139d43f81
Mang2Rang <skypopoioio@naver.com>
2023-09-15 13:59:51 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Tab } from "./components/20230911/Tab";
+import { Session1 } from "./components/20230911/Session1";
+import { List } from "./components/20230911/List";
+import { AvatarList } from "./components/20230912/AvatarList";
+import { PropsTest } from "./components/20230912/PropsTest";
+import { CounterState } from "./components/20230912/CounterState";
+import { Home } from "./components/20230913/Home";
+import { createGlobalStyle } from "styled-components";
+import { Gallery } from "./components/20230914/Gallery";
+import { OpenWeather } from "./components/20230914/OpenWeather";
+import { MyRef } from "./components/20230915/MyRef";
+import { MyRouter } from "./components/20230915/MyRouter";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  * {
+    margin : 0;
+    padding : 0;
+    box-sizing: border-box;
+    font-family: Poppins, GmarketSansMedium;
+  }
+`;
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <MyRouter />
+    </>
+  );
+}
